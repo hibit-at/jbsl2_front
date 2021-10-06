c42f2cc058a2c9ec5e0bd59596fe78cc88943c97
unknown <hibit_at@hotmail.com>
2021-10-07 08:29:02 +0900

    local password

---
+++ local_password.py
@@ -0,0 +1,16 @@
+# ローカルデバッグ用にパスワードをすべてその人の名前に
+
+import os
+import django
+import re
+import csv
+import hashlib
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jbsl2.settings')
+django.setup()
+
+from app.models import Player
+
+for p in Player.objects.all():
+    p.hashurl = p.name
+    p.save()
